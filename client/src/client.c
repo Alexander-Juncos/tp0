9b6a56869b72e89963e247be847305567d2afcb8
Alexander Juncos <alexanderljuncos@gmail.com>
2024-04-16 02:11:45 +0000

    Etapa 1 y 2: Comandos basicos

--- client/src/client.c
+++ client/src/client.c
@@ -27,8 +27,12 @@ int main(void)
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
 
-	// Loggeamos el valor de config
+	valor = config_get_string_value(config, "CLAVE");
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
 
+	// Loggeamos el valor de config
+	log_info(logger, valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -54,14 +58,26 @@ int main(void)
 
 t_log* iniciar_logger(void)
 {
-	t_log* nuevo_logger;
+	t_log* nuevo_logger = log_create("tp0.log", "NUEVO_LOG", true, LOG_LEVEL_INFO);
+
+	if (nuevo_logger == NULL) {
+		
+		perror("Error al iniciar log");
+		exit(EXIT_FAILURE);
+	}
 
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config;
+	t_config* nuevo_config = config_create("cliente.config");
+
+	if (nuevo_config == NULL) {
+
+		perror("Error al iniciar config");
+		exit(EXIT_FAILURE);
+	}
 
 	return nuevo_config;
 }
@@ -74,10 +90,15 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
+	while (strcmp(leido, "")) {
 
+		log_info(logger, leido);
+		free(leido);
+		leido = readline("> ");
+	}
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
-
+	free(leido);
 }
 
 void paquete(int conexion)
@@ -97,4 +118,6 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
 }
