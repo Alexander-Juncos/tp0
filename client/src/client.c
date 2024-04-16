f6e4d600367050310a0db2dfbcf4267f6e9beae6
Alexander Juncos <alexanderljuncos@gmail.com>
2024-04-16 18:31:22 +0000

    Etapa 3: Levantar servidor, conexion y paquetes

--- client/src/client.c
+++ client/src/client.c
@@ -46,6 +46,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor, conexion);	
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -71,7 +72,7 @@ t_log* iniciar_logger(void)
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config = config_create("cliente.config");
+	t_config* nuevo_config = config_create("/home/utnso/tp0/client/cliente.config");
 
 	if (nuevo_config == NULL) {
 
@@ -105,13 +106,22 @@ void paquete(int conexion)
 {
 	// Ahora toca lo divertido!
 	char* leido;
-	t_paquete* paquete;
+	t_paquete* paquete = crear_paquete();
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	leido = readline ("> ");
+	while (strcmp(leido, "")) {
+		agregar_a_paquete(paquete, leido, strlen(leido) + 1);// +1 para char fin str
+		free(leido);
+		leido = readline ("> ");
+	}
+	
+	// Se envia paquete al finalizar input
+	enviar_paquete(paquete, conexion); 
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
-	
+	free(leido);
+	eliminar_paquete(paquete);
 }
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
@@ -120,4 +130,5 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 	log_destroy(logger);
 	config_destroy(config);
+	liberar_conexion(conexion);
 }
